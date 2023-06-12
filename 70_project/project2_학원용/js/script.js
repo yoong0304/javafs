44b85aac763c85b6738032186b3f95b0cc05c0c3
yoong0304 <yoong0304@naver.com>
2023-06-12 17:50:47 +0900

    0612

--- 70_project/project2_학원용/js/script.js
+++ 70_project/project2_학원용/js/script.js
@@ -1,23 +1,42 @@
 // 문서가 준비되면 함수 실행
-$(function(){
-    $('#fullpage').fullpage({
-        sectionsColor: ['#FF5F45', '#0798EC', '#FC6C7C', '#FEC401'],
-        anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
-        menu: '#menu',
-        navigation: true,
-        navigationTooltips: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
-        showActiveTooltip: true,
-        // 현재 페이지를 떠날 때
-        onLeave: function(idx) {
-            if(idx == 2) {
-                $('.section3 p').animate({opacity:1, left:'50%'},700);
-            }
-        },
-        // 현재 페이지에 도착한 이후
-        afterLoad: function(e, idx) {
-            if(idx == 2) {
-                $('.section3 p').animate({opacity:0, left:-500});
-            }
+$(function () {
+    // 내비게이션바
+    $('.main > li > a').mouseenter(function (e) {
+        // a태그 기본 이벤트 제거
+        e.preventDefault();
+        $('.sub').stop().slideDown(400);
+        $('.nav_bg').stop().animate({ height: 180 }, 400);
+    });
+    $('nav').mouseleave(function () {
+        $('.sub').stop().slideUp(400);
+        $('.nav_bg').stop().animate({ height: 0 }, 400);
+    });
+
+    // 섹션1 - 비디오 자동 플레이
+    // $('video').get(0).play();
+
+    // 섹션1 - 캐로셀
+    $('.slider').bxSlider({
+        // 자동 실행
+        auto: true,
+        stopAutoOnClick: true
+        // 일시정지와 재실행 버튼
+        // autoControls: true,
+        // pager: true,
+        // slideWidth: 1000
+      });
+
+    // 풀페이지 레이아웃
+    $('.section').mousewheel(function (e, delta) {
+        let prev;
+        if (delta > 0) {
+            prev = $(this).prev().offset().top;
+            // console.log(prev);
+            $('html').stop().animate({ scrollTop: prev }, 1400, 'easeOutExpo');
+        } else if (delta < 0) {
+            let next = $(this).next().offset().top;
+            // console.log(next);
+            $('html').stop().animate({ scrollTop: next }, 1400, 'easeOutExpo');
         }
     });
 });
