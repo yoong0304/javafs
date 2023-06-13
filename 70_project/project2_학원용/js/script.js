15f0478a28b9fb352fdd43feced5ff34f8c8106b
yoong0304 <yoong0304@naver.com>
2023-06-13 17:52:51 +0900

    0613

--- 70_project/project2_학원용/js/script.js
+++ 70_project/project2_학원용/js/script.js
@@ -16,15 +16,112 @@ $(function () {
     // $('video').get(0).play();
 
     // 섹션1 - 캐로셀
-    $('.slider').bxSlider({
-        // 자동 실행
+    let slider = $('.slider').bxSlider({
+        // mode: 'horizontal',
+        // mode: 'vertical',
+        mode: 'fade',
+        // 자동 슬라이드
         auto: true,
-        stopAutoOnClick: true
-        // 일시정지와 재실행 버튼
+        // 컨트롤 버튼(좌우, 페이저)을 클릭하면 자동 스크롤 정지
+        stopAutoOnClick: true,
+        // 슬라이드 위에 hover하면 auto 일시 정지
+        autoHover: true,
+
+        // 실행/일시정지 버튼
         // autoControls: true,
-        // pager: true,
-        // slideWidth: 1000
-      });
+
+        // 내비게이션(인디케이터, 페이저)
+        pager: false,
+
+        // 이전/이후 버튼
+        controls: false,
+        // 전환 시간
+        speed: 400,
+        // 지연 시간(슬라이드가 정지되어 있는 시간)
+        pause: 3000,
+
+
+        // 슬라이드 전환 직전에 autoPager() 함수를 호출하여 동작 시킴
+        onSlideBefore: function () {
+            autoPager();
+        },
+        // 슬라이드 전환 직후 titMotion() 함수를 호출하여 동작 시킴
+        onSlideAfter: function () {
+            titMotion();
+        }
+    });
+
+    function titMotion() {
+        // 슬라이드 전환 직후의 텍스트 모션
+        $('#slideWrap .slider li div').animate({ top: -50, opacity: 1 });
+    }
+
+    function autoPager() {
+        // 페이저의 이미지 변경
+        // 페이저 a태그의 active 클래스 모두 제거
+        $('#slideWrap .pager a').removeClass('active');
+        // 현재 슬라이드 index번호를 가져와서 currentIdx에 저장
+        let currentIdx = slider.getCurrentSlide();
+        $('#slideWrap .pager a').eq(currentIdx).addClass('active');
+
+        // 슬라이드 전환 직전의 텍스트 모션
+        $('#slideWrap .slider li div').css({ top: 100, opacity: 0 });
+    }
+
+    // 페이저 버튼
+    $('#slideWrap .pager a').click(function (e) {
+        // a태그의 기본이벤트 제거
+        e.preventDefault();
+        let idx = $(this).index();
+        // idx번호에 해당하는
+        slider.goToSlide(idx);
+        return false;
+    });
+
+    // 이전 버튼
+    $('#slideWrap #prev').click(function (e) {
+        e.preventDefault();
+        // 이전 슬라이드로 이동
+        slider.goToPrevSlide();
+        autoPager();
+        return false;
+    });
+    // 이후 버튼
+    $('#slideWrap #next').click(function (e) {
+        e.preventDefault();
+        // 다음 슬라이드로 이동
+        slider.goToNextSlide();
+        autoPager();
+        return false;
+    });
+
+    // 섹션2 - ???
+    const sec2 = $('#section2'),
+        btn = sec2.find('.btn'),
+        txt1 = sec2.find('.txt1'),
+        txt2 = sec2.find('.txt2');
+
+
+    // 윈도우에 스크롤 이벤트가 발생하면 함수 실행
+    $(window).scroll(function () {
+        // 스크롤바를 스크롤한 양을 st에 지정
+        let st = $(this).scrollTop();
+        let stValue = 600;
+        console.log(st);
+
+
+        if (st >= stValue) {
+            // css(속성, 값)
+            // css({속성, 값, 속성: 값})
+            btn.css({ opacity: 1 });
+            txt1.css({ left: 360 + 'px'});
+            txt2.css({ left: 360 + 'px'});
+        } else {
+            btn.css({ opacity: 0 });
+            txt1.css({ left: -400 + 'px'});
+            txt2.css({ left: -400 + 'px'});
+        }
+    });
 
     // 풀페이지 레이아웃
     $('.section').mousewheel(function (e, delta) {
@@ -32,11 +129,11 @@ $(function () {
         if (delta > 0) {
             prev = $(this).prev().offset().top;
             // console.log(prev);
-            $('html').stop().animate({ scrollTop: prev }, 1400, 'easeOutExpo');
+            $('html').stop().animate({ scrollTop: prev }, 100, 'easeOutExpo');
         } else if (delta < 0) {
             let next = $(this).next().offset().top;
             // console.log(next);
-            $('html').stop().animate({ scrollTop: next }, 1400, 'easeOutExpo');
+            $('html').stop().animate({ scrollTop: next }, 100, 'easeOutExpo');
         }
     });
 });
